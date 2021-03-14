d4b061fbe92864d9dd3b6e7644873ae960e5cb81
NewBavli <vlad.babich.97@inbox.ru>
2021-03-14 19:41:02 +0300

    init: write code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (!bracketsConfig || !bracketsConfig.length) return 0;
+    if (!str|| !str.length) return 0;
+    bracketsConfig = bracketsConfig.map(element => element.join(''));
+    let check_braket = true;
+    while (check_braket) {
+        check_braket = false;
+      for (let i = 0; i < bracketsConfig.length; i++ ) 
+        if (str.includes(bracketsConfig[i])) {
+            check_braket = true;
+            str = str.replace(bracketsConfig[i], '')
+        }  
+    }
+    if (!str.length) return true;
+    else return false;
 }
